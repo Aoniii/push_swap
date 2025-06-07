94e113fbde48f0c37d5d3620b5ea1bd77704ab3b
Aoniii <aoniii.contact@gmail.com>
2025-06-07 23:43:09 +0200

    x

--- bonus/checker_bonus.c
+++ bonus/checker_bonus.c
@@ -41,39 +41,53 @@ static int	do_instruction(char *line, t_list **list)
 	return (0);
 }
 
+static int	loop(t_list **list, char **line, char **tmp, int *res)
+{
+	char	buffer[2];
+
+	*res = read(0, buffer, 1);
+	buffer[*res] = '\0';
+	if (*res == 0)
+		return (1);
+	else if (buffer[0] != '\n')
+	{
+		*tmp = ft_strjoin(*line, buffer);
+		if (*line)
+			free(*line);
+		*line = *tmp;
+	}
+	else
+	{
+		if (do_instruction(*line, list) == 0)
+		{
+			free(*line);
+			*line = NULL;
+			return (2);
+		}
+		free(*line);
+		*line = NULL;
+	}
+	return (0);
+}
+
 void	checker(t_list **list)
 {
 	char	*line;
 	char	*tmp;
-	char	buffer[2];
 	int		res;
+	int		ret;
 
 	res = 1;
 	line = NULL;
 	while (res > 0)
 	{
-		res = read(0, buffer, 1);
-		buffer[res] = '\0';
-		if (res == 0)
+		ret = loop(list, &line, &tmp, &res);
+		if (ret == 1)
 			break ;
-		else if (buffer[0] != '\n')
+		else if (ret == 2)
 		{
-			tmp = ft_strjoin(line, buffer);
-			if (line)
-				free(line);
-			line = tmp;
-		}
-		else
-		{
-			if (do_instruction(line, list) == 0)
-			{
-				free(line);
-				line = NULL;
-				ft_putendl_fd("Error", 2);
-				return ;
-			}
-			free(line);
-			line = NULL;
+			ft_putendl_fd("Error", 2);
+			return ;
 		}
 	}
 	if (line)
